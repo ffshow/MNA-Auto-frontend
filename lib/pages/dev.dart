1487e3f11b1429482076ba91db6bd57a1002c8ec
ibrahimSam <samai.ibra@outlook.com>
2023-09-19 10:36:22 +0100

    add faker

--- lib/pages/dev.dart
+++ lib/pages/dev.dart
@@ -1,6 +1,8 @@
+import 'package:chopper/chopper.dart';
+import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
-import 'package:mna/swagger_generated_code/client_index.dart';
-import 'package:swagger_dart_code_generator/swagger_dart_code_generator.dart';
+import 'package:mna/swagger_generated_code/swagger.swagger.dart';
+import 'package:mna/widget/widget.dart';
 
 class DevPage extends StatefulWidget {
   const DevPage({super.key});
@@ -11,6 +13,9 @@ class DevPage extends StatefulWidget {
 
 class _DevPageState extends State<DevPage> {
   late final Swagger swagger;
+  int? vehicleCount;
+  final Faker faker = Faker();
+
   @override
   void initState() {
     super.initState();
@@ -23,22 +28,86 @@ class _DevPageState extends State<DevPage> {
       appBar: AppBar(
         title: const Text('dev page'),
       ),
-      body: Column(
-        children: [
-          ElevatedButton(
-            onPressed: () {
-              swagger.apiVehicleTotalGet().then((value) {
-                print('count: ${value.body?.count}');
-              });
-
-              swagger.apiVehicleListGet().then((value) {
-                print('count: ${value.body?.data}');
-              });
-            },
-            child: const Text('get vehicles'),
-          ),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    swagger.apiVehicleTotalGet().then((value) {
+                      vehicleCount = value.body?.count;
+                      setState(() {});
+                    });
+                  },
+                  child: const Text('get vehicles count'),
+                ),
+                if (vehicleCount != null) Text('Vehicles count $vehicleCount'),
+              ],
+            ),
+            FutureBuilder(
+              future: swagger.apiVehicleListGet(),
+              builder: (
+                BuildContext context,
+                AsyncSnapshot<Response<ModelsListVehicleModel>> snapshot,
+              ) {
+                if (snapshot.hasData) {
+                  return Text(
+                    snapshot.data?.body?.data?.length.toString() ?? '0',
+                  );
+                }
+                if (snapshot.hasError) {
+                  return Text('error: ${snapshot.error}');
+                }
+                return const LoadingWidget();
+              },
+            ),
+            ElevatedButton(
+              onPressed: _createVechile,
+              child: const Text('Create random vechile'),
+            ),
+            ElevatedButton(
+              onPressed: _createOwner,
+              child: const Text('Create owner'),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  Future<void> _createVechile() async {
+    final String date = faker.date
+        .dateTime(minYear: 1999, maxYear: 2022)
+        .toUtc()
+        .toIso8601String();
+    final v = ModelsCreateVehicleModel(
+      chrono: faker.vehicle.make(),
+      collectionDate: date,
+      deliveryDate: date,
+      firstCirculation: date,
+      soldAt: date,
+      commercialName: faker.vehicle.model(),
+      currentStatus: 'toGetBack',
+      expertise: faker.randomGenerator.boolean(),
+      mileage: faker.randomGenerator.integer(1000, min: 20),
+      note: faker.lorem.sentence(),
+      ownerId: '65005460b40777ab605fa163',
+      procedureVe: faker.randomGenerator.boolean(),
+      registration: faker.vehicle.yearMakeModel(),
+      serialNumber: faker.vehicle.vin(),
+    );
+    swagger.apiVehiclePost(vehicle: v);
+  }
+
+  void _createOwner() {
+    final ModelsCreateOwnerModel o = ModelsCreateOwnerModel(
+      address: faker.address.neighborhood(),
+      email: '${faker.person.firstName()}@${faker.person.lastName()}.com',
+      phone: faker.phoneNumber.de(),
+      photo: '',
+      postalCode: faker.address.zipCode(),
+    );
+    swagger.apiOwnerPost(owner: o);
   }
 }

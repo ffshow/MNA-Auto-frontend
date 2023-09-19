c0839715162270af5180f1b60947ea7f9da122c9
ibrahimSam <samai.ibra@outlook.com>
2023-09-19 10:46:42 +0100

    add ownes page

---
+++ lib/pages/owner/data_source.dart
@@ -0,0 +1,109 @@
+import 'package:chopper/chopper.dart';
+import 'package:data_table_2/data_table_2.dart';
+import 'package:flutter/material.dart';
+import 'package:mna/swagger_generated_code/swagger.swagger.dart';
+
+class OwnerDataTableSource extends AsyncDataTableSource {
+  final Swagger service;
+
+  OwnerDataTableSource(this.service);
+
+  final List<ModelsOwnerModel> items = [];
+  bool sortAscending = false;
+  int sortColumnIndex = 3;
+  int defaultRowsPerPage = PaginatedDataTable.defaultRowsPerPage;
+  int totalCount = 0;
+
+  DataRow2 toRow(ModelsOwnerModel item) {
+    return DataRow2(
+      cells: <DataCell>[
+        DataCell(Text(item.email ?? '')),
+        DataCell(Text(item.phone ?? '')),
+        DataCell(Text(item.address ?? '')),
+        DataCell(
+          Tooltip(
+            message: item.createdAt,
+            child: Text(item.createdAt ?? ''),
+          ),
+        ),
+        DataCell(
+          Tooltip(
+            message: item.updatedAt,
+            child: Text(item.updatedAt ?? ''),
+          ),
+        ),
+        const DataCell(Row(
+          children: <Widget>[
+            IconButton(
+              // onPressed: () => service.delete(item),
+              onPressed: null,
+              icon: Icon(Icons.delete),
+            ),
+            IconButton(
+              onPressed: null,
+              icon: Icon(Icons.edit),
+            ),
+          ],
+        )),
+      ],
+    );
+  }
+
+  @override
+  bool get isRowCountApproximate => false;
+
+  @override
+  int get rowCount => totalCount;
+
+  @override
+  int get selectedRowCount => 0;
+
+  void onRowsPerPageChanged(int? value) {
+    defaultRowsPerPage = value ?? defaultRowsPerPage;
+    notifyListeners();
+  }
+
+  @override
+  Future<AsyncRowsResponse> getRows(int startIndex, int count) async {
+    final Response<ModelsListOwnerModel> res = await service.apiOwnerListGet(
+      page: (startIndex ~/ defaultRowsPerPage) + 1,
+      perPage: count,
+      sortBy: sortBy(sortColumnIndex),
+      descending: !sortAscending,
+    );
+    if (res.body?.total != null && res.body!.total != 0) {
+      totalCount = res.body!.total!;
+    }
+    return AsyncRowsResponse(
+      totalCount,
+      res.body?.data?.map((e) => toRow(e)).toList() ?? [],
+    );
+  }
+
+  void sort(int columnIndex, bool ascending) {
+    sortAscending = ascending;
+    sortColumnIndex = columnIndex;
+    refreshDatasource();
+  }
+
+  String? sortBy(int sortColumnIndex) {
+    switch (sortColumnIndex) {
+      case 0:
+        return "email";
+      case 1:
+        return "phone";
+      case 2:
+        return "address";
+      case 3:
+        return "created_at";
+      case 4:
+        return "updated_at";
+      default:
+        return "created_at";
+    }
+  }
+
+  void cancel() {
+    //TODO:
+  }
+}
--- lib/pages/owner/owner_page.dart
+++ lib/pages/owner/owner_page.dart
@@ -1,102 +1,158 @@
+import 'package:data_table_2/data_table_2.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mna/swagger_generated_code/swagger.swagger.dart';
 import 'package:mna/widget/widget.dart';
 
-import 'state/owner_cubit.dart';
+import 'data_source.dart';
 
 class OwnerPage extends StatelessWidget {
   const OwnerPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<OwnerCubit>(
-      create: (BuildContext context) => OwnerCubit(
-        RepositoryProvider.of<Swagger>(context),
-      )..getOwners(),
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Owner Page'),
-        ),
-        body: BlocBuilder<OwnerCubit, OwnerState>(
-          builder: (BuildContext context, OwnerState state) {
-            return state.when<Widget>(
-              loaded: (Iterable<ModelsOwnerModel> owners) {
-                return OwnerListWidget(owners: owners);
-              },
-              initial: () => const LoadingWidget(),
-              failed: (String error) => AppErrorWidget(
-                error: error,
-                tryAgainFunc: () {
-                  context.read<OwnerCubit>().getOwners(tryAgain: true);
-                },
-              ),
-            );
-          },
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Owners Page'),
+        centerTitle: false,
       ),
+      body: const OwnerListWidget(),
     );
   }
-
-  void _addOwner(BuildContext context) {}
 }
 
-class OwnerListWidget extends StatelessWidget {
-  const OwnerListWidget({super.key, required this.owners});
-  final Iterable<ModelsOwnerModel> owners;
+class OwnerListWidget extends StatefulWidget {
+  const OwnerListWidget({super.key});
+
+  @override
+  State<OwnerListWidget> createState() => _OwnerListWidgetState();
+}
+
+class _OwnerListWidgetState extends State<OwnerListWidget> {
+  OwnerDataTableSource? source;
+  bool sortAscending = false;
+  int sortColumnIndex = 3;
+  @override
+  void didChangeDependencies() {
+    final Swagger swagger = RepositoryProvider.of<Swagger>(context);
+    source ??= OwnerDataTableSource(swagger);
+    super.didChangeDependencies();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: PaginatedDataTable(
-        source: OwnerDataSrouce(owners),
-        columns: const [
-          DataColumn(label: Text('Email')),
-          DataColumn(label: Text('Address')),
-          DataColumn(label: Text('Created at')),
-          DataColumn(label: Text('Actions')),
+    return AsyncPaginatedDataTable2(
+      source: source!,
+      initialFirstRowIndex: 0,
+      rowsPerPage: source!.defaultRowsPerPage,
+      showCheckboxColumn: true,
+      sortAscending: sortAscending,
+      sortColumnIndex: sortColumnIndex,
+      showFirstLastButtons: true,
+      pageSyncApproach: PageSyncApproach.doNothing,
+      onRowsPerPageChanged: (int? value) {
+        source!.onRowsPerPageChanged(value);
+      },
+      loading: const LoadingWidget(),
+      empty: const Center(
+        child: Text('No data!'),
+      ),
+      horizontalMargin: 20,
+      // checkboxHorizontalMargin: 12,
+      columnSpacing: 0,
+      wrapInCard: false,
+      autoRowsToHeight: true,
+      minWidth: 800,
+      header: Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Expanded(
+            child: TextFormField(
+              expands: false,
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+                labelText: 'Search',
+                hintText: 'not implemented yet!',
+              ),
+            ),
+          ),
+          const Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                IconButton(
+                  tooltip: 'Filter',
+                  onPressed: null,
+                  icon: Icon(Icons.filter_alt),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
-    );
-  }
-}
-
-class OwnerDataSrouce extends DataTableSource {
-  final Iterable<ModelsOwnerModel> owners;
-
-  OwnerDataSrouce(this.owners);
-
-  @override
-  DataRow? getRow(int index) {
-    final ModelsOwnerModel e = owners.elementAt(index);
-    return DataRow(
-      cells: [
-        DataCell(Text(e.email ?? '')),
-        DataCell(Text(e.address ?? '')),
-        DataCell(Text(e.createdAt ?? '')),
-        DataCell(Row(
-          children: [
-            IconButton(
-              tooltip: 'Edit',
-              onPressed: () {},
-              icon: const Icon(Icons.edit),
-            ),
-            IconButton(
-              tooltip: 'Remove',
-              onPressed: () {},
-              icon: const Icon(Icons.delete),
-            ),
-          ],
-        )),
+      columns: <DataColumn2>[
+        DataColumn2(
+          label: const Text('Email'),
+          size: ColumnSize.L,
+          onSort: (int columnIndex, bool ascending) {
+            setState(() {
+              sortAscending = ascending;
+              sortColumnIndex = columnIndex;
+            });
+            source!.sort(columnIndex, ascending);
+          },
+        ),
+        DataColumn2(
+          label: const Text('Phone'),
+          size: ColumnSize.L,
+          onSort: (int columnIndex, bool ascending) {
+            setState(() {
+              sortAscending = ascending;
+              sortColumnIndex = columnIndex;
+            });
+            source!.sort(columnIndex, ascending);
+          },
+        ),
+        DataColumn2(
+          label: const Text('Address'),
+          size: ColumnSize.L,
+          onSort: (int columnIndex, bool ascending) {
+            setState(() {
+              sortAscending = ascending;
+              sortColumnIndex = columnIndex;
+            });
+            source!.sort(columnIndex, ascending);
+          },
+        ),
+        DataColumn2(
+          label: const Text('Created at'),
+          onSort: (int columnIndex, bool ascending) {
+            setState(() {
+              sortAscending = ascending;
+              sortColumnIndex = columnIndex;
+            });
+            source!.sort(columnIndex, ascending);
+          },
+        ),
+        DataColumn2(
+          label: const Text('Updated at'),
+          onSort: (int columnIndex, bool ascending) {
+            setState(() {
+              sortAscending = ascending;
+              sortColumnIndex = columnIndex;
+            });
+            source!.sort(columnIndex, ascending);
+          },
+        ),
+        const DataColumn2(
+          label: Text('Actions'),
+          size: ColumnSize.S,
+        ),
       ],
     );
   }
-
-  @override
-  bool get isRowCountApproximate => false;
-
-  @override
-  int get rowCount => owners.length;
-
-  @override
-  int get selectedRowCount => 0;
 }

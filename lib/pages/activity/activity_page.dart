4eb3db1526d8902c69697669cb3cac726b13cc77
ibrahimSam <samai.ibra@outlook.com>
2023-10-18 12:23:18 +0100

    add create activity

--- lib/pages/activity/activity_page.dart
+++ lib/pages/activity/activity_page.dart
@@ -1,6 +1,8 @@
+import 'package:chopper/src/response.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mna/cubits/cubits.dart';
+import 'package:mna/swagger_generated_code/client_index.dart';
 import 'package:mna/swagger_generated_code/swagger.models.swagger.dart';
 import 'package:mna/widget/widget.dart';
 
@@ -12,6 +14,15 @@ class ActivityPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Activities'),
+        actions: [
+          IconButton(
+            tooltip: 'Create an activity',
+            onPressed: () {
+              _createActivity(context);
+            },
+            icon: const Icon(Icons.create),
+          ),
+        ],
       ),
       body: BlocBuilder<ActivityCubit, ActivityState>(
         builder: (BuildContext context, ActivityState state) {
@@ -37,5 +48,57 @@ class ActivityPage extends StatelessWidget {
         },
       ),
     );
+  }
+
+  void _createActivity(BuildContext context) {
+    final TextEditingController controller = TextEditingController();
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Create an activity'),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Cancel'),
+            ),
+            OutlinedButton(
+              onPressed: () {
+                final value = controller.text;
+                if (value.isNotEmpty) {
+                  _create(context, value);
+                }
+                Navigator.pop(context);
+              },
+              child: const Text('Submit'),
+            ),
+          ],
+          content: TextFormField(
+            controller: controller,
+            decoration: const InputDecoration(
+              labelText: 'Label',
+            ),
+            onFieldSubmitted: (value) {
+              if (value.isNotEmpty) {
+                _create(context, value);
+              }
+            },
+          ),
+        );
+      },
+    );
+  }
+
+  Future<void> _create(BuildContext context, String value) async {
+    final Swagger swagger = RepositoryProvider.of<Swagger>(context);
+    final Response<ActivityResponse> res =
+        await swagger.apiActivityPost(activityModel: Activity(label: value));
+    if (!res.isSuccessful) {
+      // ignore: use_build_context_synchronously
+      ScaffoldMessenger.maybeOf(context)
+          ?.showSnackBar(SnackBar(content: Text(res.body.toString())));
+    }
   }
 }
